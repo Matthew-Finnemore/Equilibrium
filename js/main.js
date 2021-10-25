113611f4addb3d3a8c5eeddbeafd13112cd1b678
Matthew-Finnemore <83360160+Matthew-Finnemore@users.noreply.github.com>
2021-10-25 18:00:55 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const menuBtn = document.querySelector(".menu-btn")
 const menuItems = document.querySelector(".menu-items")
+const header = document.querySelector(".header")
 
 //main toggle
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open")
   menuItems.classList.toggle("open")
+  header.classList.toggle("open")
 })
 menuItems.addEventListener("click", () => {
   menuBtn.classList.toggle("open")
   menuItems.classList.toggle("open")
+  header.classList.toggle("open")
 })
